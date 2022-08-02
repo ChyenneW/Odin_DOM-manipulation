08c1a07b6692c1705e6b0eb13fb5758fd6ec87cd
ChyenneW <williamscheyenne771@gmail.com>
2022-08-02 10:25:41 -0500

    Create a pink div

--- index.js
+++ index.js
@@ -8,5 +8,9 @@ const blueH = document.createElement('h3');
 blueH.textContent = 'Hey, I am a blue H3!';
 blueH.setAttribute('style', 'color:blue;');
 
+const blackDiv = document.createElement('div');
+blackDiv.setAttribute('style', 'border:1px solid black; background-color:pink;');
+
 content.appendChild(redP);
 content.appendChild(blueH);
+content.appendChild(blackDiv);
