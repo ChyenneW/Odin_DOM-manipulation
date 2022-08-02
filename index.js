b0fd784b72ad33cd2da3fcd2a0d0572f8504a242
ChyenneW <williamscheyenne771@gmail.com>
2022-08-02 10:22:04 -0500

    Create a blue H3

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ const redP = document.createElement('p');
 redP.textContent = 'Hey, I am red!';
 redP.setAttribute('style', 'color:red;');
 
+const blueH = document.createElement('h3');
+blueH.textContent = 'Hey, I am a blue H3!';
+blueH.setAttribute('style', 'color:blue;');
+
 content.appendChild(redP);
+content.appendChild(blueH);
