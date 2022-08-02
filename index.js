efce833d0146f3a6d71a90c1d0dd09dd8685592d
ChyenneW <williamscheyenne771@gmail.com>
2022-08-02 10:18:41 -0500

    Change redP font color to red

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const content = document.querySelector('.content');
 
 const redP = document.createElement('p');
 redP.textContent = 'Hey, I am red!';
+redP.setAttribute('style', 'color:red;');
 
 content.appendChild(redP);
